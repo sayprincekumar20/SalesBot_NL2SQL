3413df6a04fdade8865c58ed574779377692e19d
prince kumar <142137873+sayprincekumar20@users.noreply.github.com>
2025-10-17 01:51:08 +0530

    api_frontend_update

--- frontend/src/components/QueryForm.jsx
+++ frontend/src/components/QueryForm.jsx
@@ -94,7 +94,7 @@ const QueryForm = () => {
 
     setLoading(true);
     try {
-      const res = await fetch("http://127.0.0.1:8000/query", {
+      const res = await fetch("http://65.2.168.102:8000/query", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt }),
